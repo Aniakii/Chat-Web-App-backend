761e704b6b883e477146d7164a9901b9473bacb1
Aniakii <prezes.kot.ani@gmail.com>
2025-04-04 13:35:17 +0200

    Add more urls to cors configuration

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using FormulaOne.ChatService.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 
 builder.Services.AddSignalR();
@@ -11,11 +13,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var publicIp = Environment.GetEnvironmentVariable("PUBLIC_IP") ?? "localhost";
+
 builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("AllowSpecificOrigin", builder =>
     {
-        builder.WithOrigins("http://192.168.1.103:8080", "http://localhost:8080", "http://localhost:3000", "http://127.0.0.1:3000")
+        builder.WithOrigins("http://192.168.1.103:8080", "http://localhost:8080", "http://localhost:3000", "amajka.us-east-1.elasticbeanstalk.com:8080", "amajka.us-east-1.elasticbeanstalk.com:3000" "http://127.0.0.1:3000", $"http://{publicIp}:3000")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
